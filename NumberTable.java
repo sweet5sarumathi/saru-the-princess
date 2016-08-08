10059ff8f877785289b03ea5d7a1526424125d1a
sweet5sarumathi <sowmia005@gmail.com>
2016-08-08 23:41:41 +0530

    Create NumberTable.java

---
+++ NumberTable.java
@@ -0,0 +1,14 @@
+import java.util*;
+class NumberTable
+{
+  public static void mian(String args[])
+  {
+   int i,j,k;
+   for(i=1,k=1;k<=5;k++)
+   {
+    for(j=1;j<=k;i++,j++)
+    System.out.print(i+"\t");
+    System.out.println("\n");
+    }
+  }
+}
